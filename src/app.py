3d6fb31833009624ceec33260913ec115720a65a
Dan Bially Levy <dbiallyl@macalester.edu>
2023-11-14 13:11:17 -0600

    Added start of post template

--- src/app.py
+++ src/app.py
@@ -3,5 +3,11 @@ from flask import Flask, render_template, make_response
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
+
+# Maybe we will want to pass in a post object with all info to put in Jinja?
+# ohhh how are we gonna store all these formatting things like order of elements and such....
+@app.route("/<postName>")
+def viewPost(postName):
+    return render_template("post.html", postName=postName)
