ebbc2936fe8ce09ee2138133ed6dd2b52d2a333a
Dan Bially Levy <dbiallyl@macalester.edu>
2023-11-16 14:24:06 -0600

    Stopped page reloads on search, started looking into Spotify embeds
    Currently can only play 30 second clips on embeds, but I'm seeing some promising documentation for choosing points to play from!

--- src/static/script.js
+++ src/static/script.js
@@ -4,9 +4,10 @@ function addElement() {
     $("#addChoice").show()
 }
 
+// Coded with help from: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/search
 function addMusic() {
     $("#addChoice").hide()
-    $("<form onsubmit=\"findSong()\"><input name=\"songSearch\" type=\"text\"><input type=\"submit\" value=\"Search\"></form>").insertBefore("#addDiv")
+    $("<search><form id=\"songSearchForm\" onsubmit=\"findSong()\"><input name=\"songSearch\" type=\"search\" placeholder=\"Search...\"</form><input type=\"button\" value=\"Search\" onclick=\"doSearch()\"></search>").insertBefore("#addDiv")
 }
 
 function addText() {
@@ -16,7 +17,18 @@ function addText() {
     $("<p id=\"" + textId + "\" contenteditable data-placeholder=\"Start Typing...\"></p><br/>").insertBefore("#addDiv")
 }
 
-// Submitting form currently clears entire page!!!!!
-function findSong() {
-    alert("You submitted the search");
+// will probably be helpful https://stackoverflow.com/questions/18169933/submit-form-without-reloading-page
+function doSearch() {
+    alert("You submitted the search")
+
+    // put in code here to do the API request, should return a "tracks" object that contains an array of tracks
+    // temporary tracks array below
+    tracks = ["track1", "track2"]
+
+    // useful for embeds: https://developer.spotify.com/documentation/embeds/references/iframe-api
+    // has a seek function to seek certain point in song!!!
+    spotifyID = "5omLfecV0S68gitZpQpMjQ"
+    // only able to play 30 seconds right now....
+    exampleEmbed = "<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/"+spotifyID+"?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>"
+    $(exampleEmbed).insertBefore("#addDiv")
 }
