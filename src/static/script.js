506ff69c771dc95631cb0f16702d6ce755ecbea1
Dan Bially Levy <dbiallyl@macalester.edu>
2023-11-18 10:57:58 -0600

    Finish post button brings up finish form

--- src/static/script.js
+++ src/static/script.js
@@ -32,3 +32,7 @@ function doSearch() {
     exampleEmbed = "<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/"+spotifyID+"?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>"
     $(exampleEmbed).insertBefore("#addDiv")
 }
+
+function finishPost() {
+    $("#finishForm").show()
+}
