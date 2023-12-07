95ac4d8b27d06daa77ee093ef90485798ca6174e
Dan Bially Levy <dbiallyl@macalester.edu>
2023-12-07 14:34:55 -0600

    Fake back button now works, needs accessibility

--- src/static/script.js
+++ src/static/script.js
@@ -10,23 +10,15 @@ loginButton.onclick = function() {
     location.assign("/login");
 }
 
-var backButton = document.getElementById('backButton');
-backButton.onclick = function() {
-    location.assign("../");
-}
+// var backButton = document.getElementById('backButton');
+// backButton.onclick = function() {
+//     location.assign("../");
+// }
 
 var logoutButton = document.getElementById('logout');
 logoutButton.onclick = function() {
     location.assign("/logout");
 }
-
-
-
-// TODO: Not working, not sure why
-// var createAccount = document.getElementById('newAccount');
-// createAccount.onclick = function() {
-//     location.assign("/newAccount");
-// }
 
 // Coded with help from: https://stackoverflow.com/questions/178325/how-do-i-check-if-an-element-is-hidden-in-jquery
 function clickAdd() {
@@ -88,35 +80,23 @@ function addSong(youtubeID, musicId) {
         height: '390',
         width: '640',
         videoId: youtubeID,
-        // events: {
-        //     'onStateChange': onPlayerStateChange
-        // },
-        // playerVars: {
-        //     'start': 35
-        // }
     });
-
-    // player.onclick = clickPlayer;
 
     players.push(player)
     playersTime.push(player.getCurrentTime())
-
-    
-    // I think the below will be the function we need to change start time dynamically (documentation: https://developers.google.com/youtube/iframe_api_reference#Playback_controls)
-    // player.seekTo(35, true)   
 }
 
-// doesn't work within iframe of course. 
-function clickPage(){
-    // window.alert("Clicked!")
-    for (i = 0; i < players.length; i++) {
-        if (players[i].getPlayerState() == 2) {
-            if (playersTime[i] != players[i].getCurrentTime()) {
-                window.alert("New time!")
-            } 
-        }
-    }
-}
+// // doesn't work within iframe of course. 
+// function clickPage(){
+//     // window.alert("Clicked!")
+//     for (i = 0; i < players.length; i++) {
+//         if (players[i].getPlayerState() == 2) {
+//             if (playersTime[i] != players[i].getCurrentTime()) {
+//                 window.alert("New time!")
+//             } 
+//         }
+//     }
+// }
 
 function addComment(idNum) {
     // not sure why but it wont let me make the finish comment button an input.... (in or out of form!!!)
@@ -146,91 +126,9 @@ function finishComment(idNum) {
     // will need to get comment info and start of comment time
 }
 
-
-// function clickPlayer() {
-//     window.alert("Clicked")
-//     // player = players[playerNum]
-//     // if (player && player.getPlayerState() == 2) {
-//     //     playerTime = player.getCurrentTime()
-//     //     if (lastPlayerTime[playerNum] != playerTime) {
-//     //         window.alert(player.getCurrentTime())
-//     //     }
-//     //     lastPlayerTime[playerNum] = playerTime
-//     // }
-// }
-
-// function onPlayerStateChange(event) {
-//     // currTime = players[0].getCurrentTime()
-//     // if(event.data==2) {
-//     //     window.alert(players[0].getCurrentTime())
-//     // }
-//     // window.alert("Curr: ")
-// }
-
 function onYouTubeIframeAPIReady() {
     console.log("youtube ready")
 }
-// window.onSpotifyWebPlaybackSDKReady = () => {
-//     const token = getAccessKey();
-//     const player = new Spotify.Player({
-//       name: 'Web Playback SDK Quick Start Player',
-//       getOAuthToken: cb => { cb(token); },
-//       volume: 0.5
-//     });
-
-//     // Ready
-//     player.addListener('ready', ({ device_id }) => {
-//     console.log('Ready with Device ID', device_id);
-//     });
-    
-//     // Not Ready
-//     player.addListener('not_ready', ({ device_id }) => {
-//     console.log('Device ID has gone offline', device_id);
-//     });
-    
-//     player.addListener('initialization_error', ({ message }) => {
-//         console.error(message);
-//     });
-    
-//     player.addListener('authentication_error', ({ message }) => {
-//         console.error(message);
-//     });
-    
-//     player.addListener('account_error', ({ message }) => {
-//         console.error(message);
-//     });
-    
-//     player.connect();
-// }
-
-// async function getAccessKey() {
-//     const client_id = "b7bc1b3b25c64838b631dcd8fbda3894";
-//     const client_secret = "3ca65cbb7c374c0da726c9cca1e9da57";
-//     const options = {
-//         url: "https://accounts.spotify.com/api/token",
-//         method: "POST",
-//         headers: {
-//             "Authorization":"Basic " + btoa(client_id + ":" + client_secret),
-//             "Content-Type":"application/x-www-form-urlencoded",
-//         },
-//         body:"grant_type=client_credentials&client_id=" + client_id +"&client_secret=" + client_secret,
-//         json:true
-//     };
-//     const response = await fetch("https://accounts.spotify.com/api/token", options);
-    
-//     const token = await response.json();
-
-//     return token["access_token"]
-// };
-
-// from https://www.w3schools.com/howto/howto_css_modals.asp
-// // When the user clicks anywhere outside of the modal, close it
-// finishBox = document.getElementById("finishDiv")
-// window.onclick = function(event) {
-//     if (event.target == finishBox) {
-//         finishBox.style.display = "none"
-//     }
-// }
 
 function finishPost() {
     $("#finishDiv").show()
