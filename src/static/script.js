cf3c243742c905c64cfb5a84a62b95ddac477bc7
Dan Bially Levy <dbiallyl@macalester.edu>
2023-12-17 23:59:17 -0600

    Fixed minor create post bugs

--- src/static/script.js
+++ src/static/script.js
@@ -228,11 +228,10 @@ function addSong(youtubeID, blockId) {
     idNum = blockId.substring(6)
     musicId = "music" + idNum
     // Help from: https://stackoverflow.com/questions/8488005/how-to-put-a-jpg-or-png-image-into-a-button-in-html
-    exampleEmbed = "<div id=\"player-" + musicId + "\"></div><div class='commentsDiv'><img id='viewComments-" + musicId + "' role='button' src='../static/images/triangle.png' alt='a simple arrow'><p>View Comments</p><br/><div id='comments-"+musicId+"' class='comments-music'></div><input type='image' role='button' src='../static/images/addComment.png' alt='Add Comment' id='button-" + musicId + "' class='commentButton' onclick='addComment(" + idNum + ")'></button></div>"
+    exampleEmbed = "<div id=\"player-" + musicId + "\"></div><div class='commentsDiv'><img onclick='viewComments(" + idNum + ")' id='viewComments-" + musicId + "' role='button' src='../static/images/triangle.png' alt='a simple arrow'><p>View Comments</p><br/><div id='comments-"+musicId+"' class='comments-music'></div><input type='image' role='button' src='../static/images/addComment.png' alt='Add Comment' id='button-" + musicId + "' class='commentButton' onclick='addComment(" + idNum + ")'></button></div>"
     $(".results").remove()
     $(exampleEmbed).insertAfter("search")
     $("search").remove()
-    $("#viewComments-" + musicId).on("click", (e) => viewComments(idNum))
 
     // Players must be referenced to interact with music video
     var player = createPlayer(idNum, youtubeID);
@@ -257,6 +256,8 @@ function addComment(idNum) {
     $(".timestampButton").on("click", () => setTimestamp(idNum))
 
     $("#commentForm" + idNum).on("submit", (e) => finishComment(e, idNum))
+
+    $("#delete-"+idNum).height($("#block-"+idNum).height())
 }
 
 /** 
@@ -301,6 +302,8 @@ function finishComment(e, idNum) {
     player = players[idNum]
     $("#button-music" +idNum).show()
     $(".commentForm").remove()
+
+    $("#delete-"+idNum).height($("#block-"+idNum).height())
 }
 
 /** 
